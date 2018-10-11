d0605e59543200718ea41bf9462c4f3fb72a758e
Lincoln Anderson <landerson@352inc.com>
2018-10-10 22:30:36 -0400

    Install cors for functions.

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,8 @@
 const functions = require('firebase-functions');
+const cors = require('cors')({ origin: true });
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+  cors(request, response, () => {
+    response.send("Hello from Firebase (and CORS)!");
+  });
 });
