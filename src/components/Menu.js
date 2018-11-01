0d0178bb7aaf0128fb2d122ba3e9dec08c837c14
Lincoln Anderson <landerson@352inc.com>
2018-10-31 23:06:57 -0400

    Add menu icons.

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import map from 'lodash/map';
+import { renderComponent } from 'recompose';
+
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-import { renderComponent } from 'recompose';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import Avatar from '@material-ui/core/Avatar';
+import Divider from '@material-ui/core/Divider';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
 import { auth } from '../firebase';
 import { mainMenu } from '../menus';
@@ -17,18 +22,21 @@ const renderLink = renderComponent(Link)();
 const Menu = withUserOnly(({ user }) => (
   <List>
     <ListItem>
+      <Avatar alt={user.displayName} src={user.photoURL} />
       <ListItemText
         primary={`Logged in as ${user.displayName}`}
         secondary={`UID: ${user.uid}`}
       />
     </ListItem>
-    <ListItem><img width={80} src={user.photoURL} alt={user.displayName} /></ListItem>
-    {map(mainMenu.routes, ({ title, path}, index) => (
+    <Divider />
+    {map(mainMenu.routes, ({ title, path, icon }, index) => (
         <ListItem key={index} button component={renderLink} to={path()}>
+          <ListItemIcon>{icon}</ListItemIcon>
           <ListItemText primary={title} />
         </ListItem>
     ))}
     <ListItem button onClick={handleClickSignOut}>
+      <ListItemIcon><ExitToAppIcon /></ListItemIcon>
       <ListItemText primary="Log out" />
     </ListItem>
   </List>
